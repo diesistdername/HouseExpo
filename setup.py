db08021cf4c87a8a9b8eb254a113a913a1a6ef76
l.thyssen <l.thyssen@roometric.com>
2023-04-29 14:13:41 +0200

    fix build error for getting started

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup
 setup(name='pseudoslam',
       version='0.0.1',
       description='A reinforcement learning environment for robot autonomous exploration',
-      install_requires=['gym', 'numpy', 'pyyaml', 'matplotlib', 'opencv-python']
+      install_requires=['gym', 'numpy', 'pyyaml', 'matplotlib', 'opencv-python'],       
+      packages=['HouseExpo']
       )
